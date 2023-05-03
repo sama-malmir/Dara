71202fc3fe563838b18e05ab0f047d4cfa7a9f10
sama-malmir <malmir.sama1@gmail.com>
2023-05-03 14:49:54 +0430

    buttom delete

--- src/components/MT.vue
+++ src/components/MT.vue
@@ -1,11 +1,9 @@
 <template>
     <div class="flex justify-center">
-        <div class="style">
-
-
+        <div class="style w-3/4">
             <div class="inline-flex items-center flex-col">
                 <label>دسته‌بندی</label>
-                <div  class="flex items-center mr-14 border my-3 py-3 px-4 rounded-xl h-8 bg-white text-main-color w-48" > {{ dastebandiha }} </div>
+                <div  class="flex items-center mr-14 border my-3 py-3 px-4 rounded-xl h-8 bg-white text-main-color w-40" > {{ dastebandiha }} </div>
             </div>
             <div class="inline-flex items-center flex-col">
                 <label>تاریخ</label>
@@ -19,6 +17,10 @@
                 <label >توضیحات</label>
                 <div class="flex items-center mr-14 border my-3 py-3 px-4 rounded-xl h-8 bg-white text-main-color w-48"> {{ tozihat }}</div>
             </div>  
+            <div class=" flex items-center mr-8">
+                <button class="h-8 border w-16 rounded-xl  bg-secondary-color text-main-color font-bold mt-10" @click="deleteTransaction()">حذف</button>
+
+            </div>
         </div>
     </div>
 </template>
@@ -36,16 +38,19 @@ props:{
     tarikh:String,
 },
 methods:{
-   
+    deleteTransaction(){
+
+        
+    }
 }
 }
 </script>
 <style scoped>
 .style{
-    @apply  bg-main-color mr-72 h-32   border rounded-xl -mt-435 px-3 pt-8 text-secondary-color flex flex-row
+    @apply  bg-main-color mr-52 h-32   border rounded-xl -mt-435 px-1 pt-4 text-secondary-color flex flex-row
 }
 .stylelists{
-    @apply border my-3 py-3 px-4 rounded-full  inline-flex justify-between
+    @apply border my-3 py-3 px-4 rounded-full  inline-flex justify-around
 }
 
 </style>
